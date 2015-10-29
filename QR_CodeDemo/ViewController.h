7629caf921698ad1ece67e7e85a9b228f8508b0c
brian-chu <chuchengpeng@liqunfeidemac-mini.local>
2015-10-29 15:38:35 +0800

    Initial Commit

---
+++ QR_CodeDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QR_CodeDemo
+//
+//  Created by liqunfei on 15/10/29.
+//  Copyright © 2015年 chuchengpeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
